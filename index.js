08bd02366433a085d2f202acb7e449d497539c35
shariyer <s.m.shariyer04@gmail.com>
2022-11-24 21:15:48 +0600

    database connection added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,6 +12,23 @@ app.use(express.json());
 // requiring jwt and dotenv
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+
+// data base connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dhtiicz.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
 
 // root api
 app.get("/", (req, res) => {
